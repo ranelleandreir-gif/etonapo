a49089b80c8a954ba0e4f6ace02cfc97fd508fd2
ranelleandreir-gif <ranelleandreir@gmail.com>
2026-04-20 21:31:37 +0800

    Add files via upload

--- admin.js
+++ admin.js
@@ -17,3 +17,19 @@ window.checkAccess = async function () {
     document.getElementById("msg").innerText = "Invalid Access Key!";
   }
 };
+window.checkAccess = function () {
+  let key = document.getElementById("accessKey").value;
+
+  // 👉 palitan mo kung anong gusto mong key
+  let correctKey = "kasipag123";
+
+  if (key === correctKey) {
+    // save access (optional, for protection later)
+    localStorage.setItem("adminAccess", "granted");
+
+    // redirect to admin login
+    window.location.href = "admin-login.html";
+  } else {
+    document.getElementById("msg").innerText = "Wrong Access Key!";
+  }
+};
